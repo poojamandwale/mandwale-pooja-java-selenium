99637538e4dab8236bc22b1aba900db3f752ba7a
poojamandwale <pooja.mandwale31@gmail.com>
2018-11-02 15:04:59 +0530

    my first commit

---
+++ src/FirstClass.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	float r=3.0f;
+	float pi=3.14f;
+	float area=pi*r*r;
+	System.out.println(area);
+	
+	
+	}
+
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,16 @@
+
+public class SecondClass {
+	public static void main(String[] args){
+		int a,b,temp;
+		a=10;
+		b=20;
+		System.out.println("first variable   " +a);
+		System.out.println("second variable  "+b);
+		temp=10;
+		a=b;
+		b=temp;
+		System.out.println("swapped variable a and b  " +a +"   "+ +b);
+		//System.out.println("second variable  "+b);
+	}
+
+}
